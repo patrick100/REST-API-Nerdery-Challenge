785d2399f694f1ec08d76d718ba6771238b7b3f8
Patrick Lazo <patrickdz96@gmail.com>
2021-10-13 17:49:14 -0500

    Update email.ts

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -2,7 +2,7 @@ import sgMail from '@sendgrid/mail';
 import Email from '../interfaces/email.interface';
 
 export const sendEmail = async (emailData: Email): Promise<void> => {
-  console.log('SENDGRID_API_KEY: ', process.env.SENDGRID_API_KEY);
+  console.error('SENDGRID_API_KEY: ', process.env.SENDGRID_API_KEY);
   sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
 
   const msg = {
