ca7c5c80e22954872681fe91747259c630a92be2
Kevin Cotrina <kcotrinam92@gmail.com>
2021-10-13 17:38:48 -0500

    Add post routes

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,13 @@
 import express, { Router } from 'express';
 import { usersRoutes } from './routes/users.route';
+import { postsRoutes } from './routes/posts.route';
 import swaggerUi from 'swagger-ui-express';
 const expressRouter = express.Router();
 const swaggerSpec = require('../docs/configuration');
 
 export function router(app: Router): Router {
   app.use('/api/v1/users', usersRoutes());
+  app.use('/api/v1', postsRoutes());
   app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
   return expressRouter;
 }
---
+++ src/routes/posts.route.ts
@@ -0,0 +1,24 @@
+import express, { Router } from 'express';
+import asyncHandler from 'express-async-handler';
+import {
+  find,
+  create,
+  findOne,
+  findUserPosts,
+  update,
+  deletePost,
+  findMyPosts,
+} from '../controllers/posts.controller';
+
+const router = express.Router();
+
+export function postsRoutes(): Router {
+  router.route('/posts').get(asyncHandler(find)).post(create);
+  router.route('/posts/:uuid').get(asyncHandler(findOne)).patch(update);
+  router.route('/accounts/me/posts').get(asyncHandler(findMyPosts));
+  router.route('/accounts/:accountUuid/posts/:uuid').delete(asyncHandler(deletePost));
+  router.route('/accounts/me/posts/:uuid').delete(asyncHandler(deletePost));
+  router.route('/accounts/:accountUuid/posts').get(asyncHandler(findUserPosts));
+
+  return router;
+}
