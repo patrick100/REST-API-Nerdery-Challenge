04963ee7bad1a235213c980e35efe1d77cbbe97b
Patrick Lazo <patrickdz96@gmail.com>
2021-10-13 19:01:41 -0500

    Update email.ts

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -1,14 +1,14 @@
 import sgMail from '@sendgrid/mail';
 import Email from '../interfaces/email.interface';
-require('dotenv').config();
+//require('dotenv').config();
 
 export const sendEmail = async (emailData: Email) => {
   //const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
 
-  const apiKey = `${process.env.SENDGRID_API_KEY}`;
-  console.log('SendGrid key ', apiKey);
-
-  sgMail.setApiKey('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');
+  //const apiKey = `${process.env.SENDGRID_API_KEY}`;
+  //console.log('SendGrid key ', apiKey);
+  const SENDGRID_API_KEY = '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';
+  sgMail.setApiKey(SENDGRID_API_KEY);
 
   const msg = {
     to: emailData.email,
