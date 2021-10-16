ac90278f1735994cdb2c9f372f7562871955d7d9
Patrick Lazo <patrickdz96@gmail.com>
2021-10-16 00:11:16 -0500

    feat: add reports

---
+++ src/controllers/reports.controller.ts
@@ -0,0 +1,41 @@
+import { plainToClass } from 'class-transformer';
+import { Request, Response } from 'express';
+import { ReportsService } from '../services/reports.service';
+import { CreateReportDto } from '../dtos/reports/request/create-report.dto';
+import { ReportDto } from '../dtos/reports/response/report.dto';
+import RequestWithUserId from '../interfaces/requestWithUserId.interface';
+import { convertToJson, responseApi } from '../utils/serializer';
+
+export async function reportPost(expressRequest: Request, res: Response): Promise<void> {
+  const req = expressRequest as RequestWithUserId;
+  const dto = plainToClass(CreateReportDto, {
+    title: req.body.title,
+    description: req.body.description,
+    resourceId: +req.params.postId,
+    userId: +req.userId,
+    type: 'POST',
+  });
+  await dto.isValid();
+  const report = await ReportsService.reportPost(dto);
+
+  const reportJson = convertToJson(plainToClass(ReportDto, report));
+
+  res.status(201).json(responseApi(reportJson));
+}
+
+export async function reportComment(expressRequest: Request, res: Response): Promise<void> {
+  const req = expressRequest as RequestWithUserId;
+  const dto = plainToClass(CreateReportDto, {
+    title: req.body.title,
+    description: req.body.description,
+    resourceId: +req.params.commentId,
+    userId: +req.userId,
+    type: 'COMMENT',
+  });
+  await dto.isValid();
+  const report = await ReportsService.reportComment(dto);
+
+  const reportJson = convertToJson(plainToClass(ReportDto, report));
+
+  res.status(201).json(responseApi(reportJson));
+}
--- src/router.ts
+++ src/router.ts
@@ -5,6 +5,7 @@ import { authRoutes } from './routes/auth.route';
 import { likesRoutes } from './routes/likes.route';
 import swaggerUi from 'swagger-ui-express';
 import { commentsRoutes } from './routes/comments.route';
+import { reportsRoutes } from './routes/reports.route';
 const expressRouter = express.Router();
 import swaggerSpec from './docs/configuration';
 
@@ -14,6 +15,7 @@ export function router(app: Router): Router {
   app.use('/api/v1/accounts', usersRoutes());
   app.use('/', authRoutes());
   app.use('/api/v1', likesRoutes());
+  app.use('/api/v1', reportsRoutes());
   app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
   return expressRouter;
 }
