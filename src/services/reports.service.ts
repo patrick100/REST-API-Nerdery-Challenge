711c53ed7f47c8330ee758a532d8f940df421fc7
Patrick Lazo <patrickdz96@gmail.com>
2021-10-16 00:34:23 -0500

    feat: add report test

--- src/services/reports.service.ts
+++ src/services/reports.service.ts
@@ -5,7 +5,7 @@ import createError from 'http-errors';
 
 export class ReportsService {
   static async reportPost(input: CreateReportDto): Promise<Report> {
-    const post = prisma.post.findUnique({ where: { id: input.resourceId } });
+    const post = await prisma.post.findUnique({ where: { id: input.resourceId } });
 
     if (!post) throw createError(404, 'Not Found Post');
 
@@ -13,7 +13,7 @@ export class ReportsService {
   }
 
   static async reportComment(input: CreateReportDto): Promise<Report> {
-    const comment = prisma.comment.findUnique({ where: { id: input.resourceId } });
+    const comment = await prisma.comment.findUnique({ where: { id: input.resourceId } });
 
     if (!comment) throw createError(404, 'Not Found Comment');
 
